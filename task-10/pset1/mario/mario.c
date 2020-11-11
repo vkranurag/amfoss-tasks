3c15bea85d3f24a972c19eb49f0e70be6af58ee7
Ragify <73241561+Ragify@users.noreply.github.com>
2020-11-11 21:37:34 +0530

    Add files via upload

--- task-10/pset1/mario/mario.c
+++ task-10/pset1/mario/mario.c
@@ -1,1 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int h;
+    do
+    {
+        h =get_int("Height: ");
+    }
+    while (h < 2 || h > 8);
+    for (int i = 0; i < h; i++)
+    {
+	    for (int j = 0; j < h; j++)
+        {
+            if (j < (h - i - 1))
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+}
